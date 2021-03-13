13023cc0e50d529aa720f98a93d837a1cbecf5df
andrew.berry <andrew.berry@6162s.net>
2021-03-13 10:21:20 +0000

    Cleaning up script

--- build.sh
+++ build.sh
@@ -1,17 +1,25 @@
-#First we set up an openvpn3 client
 #Script only works for Centos7
-read -p 'New Hostname for machine (must be all lowercase eg "jump.clientname.6162s.net": ' hostname
+
+
+#Needs to have a compliant hostname set
+read -p 'New Hostname for machine (must be all lowercase eg "clientname.6162s.net": ' hostname
 hostname $hostname
 echo $hostname > /etc/hostname
+
+#Installing openvpn3 and connecting to the jump-box network
 yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 yum install yum-plugin-copr -y
 yum copr enable dsommers/openvpn3 -y
 yum install openvpn3-client -y
 openvpn3 session-start --config Jump_Network.ovpn
+
+#Adding the internal DNS resolver and joining the domain
 echo 'nameserver 10.0.10.1' > /etc/resolv.conf
 yum install realmd ipa-client oddjob oddjob-mkhomedir sssd -y
 read -p 'Joining the domain (Must domain admin) Username: ' dadmin
 realm join -v auth.6162s.net -U $dadmin
+
+#Installing access software and fixing up some by-default broken xrdp configs
 yum install cockpit NetworkManager -y
 yum groupinstall "GNOME Desktop" "Graphical Administration Tools" -y
 yum groupinstall -y "Xfce"
@@ -19,6 +27,8 @@ yum install xrdp tigervnc-server -y
 mv sesman.ini /etc/xrdp/sesman.ini
 systemctl start xrdp
 systemctl start cockpit
+
+#Firewall configs - Not working right just yet.
 read -p 'Configuring firewall. Please enter WAN interface (usually eth0) : ' interface
 firewall-cmd --zone=public --add-source=10.0.8.0/24 --permanent
 firewall-cmd --zone=public --add-source=10.0.9.0/24 --permanent
